f304bc7d383756719036cecd6e6d2c14eb5ac276
Rocky9852 <abhisheklkr628@gmail.com>
2025-02-10 23:43:55 +0530

    Update play.py

--- DnsXMusic/utils/inline/play.py
+++ DnsXMusic/utils/inline/play.py
@@ -39,45 +39,6 @@ def get_progress_bar(percentage):
     else:
         return "───────────"
 
-def get_progress_bare(percentage):
-    umm = math.floor(percentage)
-
-   
-    if 0 < umm <= 10:
-        return "♨ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 𝐁𝖾𝗌𝗍 𝐅𝖾α𝗍υ𝗋𝖾𝗌 ♨"
-    elif 5 <= umm < 20:
-        return "♨ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 𝐅α𝗏ⱺ𝗋𝗂𝗍𝖾 ρᥣα𝗒ᥣ𝗂𝗌𝗍 ♨"
-    elif 10 <= umm < 30:
-        return "♨ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 𝐌𝗂ᥣᥣ𝗂ⱺ𐓣 𝐒ⱺ𐓣𝗀𝗌 ♨"
-    elif 15 <= umm < 40:
-        return "♨ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 𝐄α𝗌𝗂ᥣ𝗒 𝐒𝗍𝗋𝖾αꭑ ♨"
-    elif 20 <= umm < 50:
-        return "♨ 𝐋ⱺω-𝐒ρ𝖾𝖾ᑯ 𝐒𝗍𝗋𝖾αꭑ𝗂𐓣𝗀 ♨"
-    elif 25 <= umm < 60:
-        return "♨ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 𝐁𝗂𝗀 𝐃α𝗍αᑲα𝗌ɦ ♨"
-    elif 30 <= umm < 70:
-        return "♨ 𝐅𝗋𝖾𝖾 𝐃ⱺω𐓣ᥣⱺαᑯ 𝐌υ𝗌𝗂𝖼 ♨"
-    elif 35 <= umm < 80:
-        return "♨ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 𝐅α𝗏ⱺ𝗋𝗂𝗍𝖾 ρᥣα𝗒ᥣ𝗂𝗌𝗍 ♨"
-    elif 40 <= umm < 90:
-        return "♨ 𝐋α𝗀 𝐅𝗋𝖾𝖾 𝐆ααɳα 𝐌υ𝗌𝗂𝖼 ♨"
-    elif 45 <= umm < 100:
-        return "♨ 𝐌υѕι𝚌 𝐒𝗍υᑯ𝗂ⱺ ♨"
-    else:
-        return "♨ 𝐓ɦ𝖾 𝐒ⱺ𐓣𝗀 𝚰𝗌 𝐎𝗏𝖾𝗋 ♨"
-
-def get_progress_baree(percentage):
-    umm = math.floor(percentage)
-    
-    if 0 < umm <= 100:
-        return "᪥⋟ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 ⋞᪥"
-    elif 5 <= umm < 200:
-        return "᪥⋟ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 ⋞᪥"
-    else:
-        return "᪥⋟ 𝐃ɳѕ 𝐌υ𝗌𝗂𝖼 ⋞᪥"
-        
-        
-        
 def stream_markup_timer(_, videoid, chat_id, played, dur):
     played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
