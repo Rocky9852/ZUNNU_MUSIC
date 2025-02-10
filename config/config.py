e0f79a033c02effc3f632bb25cd9b643ce8e2cd6
Rocky9852 <abhisheklkr628@gmail.com>
2025-02-10 21:18:53 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/Rocky9852/Extra_plugins",
+    "https://github.com/Rocky9852/FCC_repo",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
